310617cdfaac04c914638a24dc426b6cac38ebf6
Ron Wexler <ronmada@gmail.com>
2020-10-06 22:35:12 +0300

    client work

--- client/health-statement-app/src/app/app.module.ts
+++ client/health-statement-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignatureFormComponent } from './components/signature-form/signature-form.component';
+import { MainFormComponent } from './components/main-form/main-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignatureFormComponent,
+    MainFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/health-statement-app/src/app/components/main-form/main-form.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-form',
+  templateUrl: './main-form.component.html',
+  styleUrls: ['./main-form.component.css'],
+})
+export class MainFormComponent implements OnInit {
+  booly = false;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
