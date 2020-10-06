aad8e9687ad7ce91f2f2dbf15defa2a0f8e31354
Ron Wexler <ronmada@gmail.com>
2020-10-06 17:43:57 +0300

    server listen and timestamper

--- server/app.js
+++ server/app.js
@@ -1,14 +1,24 @@
-require('dotenv').config();
-const mongoConnection = require('./mongoConnection');
-const express = require('express');
-const cors = require('cors');
+require('dotenv').config()
+const timeStampFunc = require('./timeStampFunc')
+const mongoConnection = require('./mongoConnection')
+const express = require('express')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(express.json()); // Make sure it comes back as json
+app.use(express.json()) // Make sure it comes back as json
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 
-app.use(cors());
+app.use(cors())
 
-mongoConnection();
+mongoConnection()
+
+app.use(timeStampFunc)
+
+app.get('/', (req, res) => {
+  res.status(200).send(`<h7>${req.hostname}</h7>`)
+})
+
+const PORT = process.env.PORT || 5000 //set port
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`)) //start server
---
+++ server/models/form.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const formSchema = new mongoose.Schema({
+  formType: String,
+  name: String,
+  id: String,
+  institute: String,
+  phoneNum: String,
+  guardianId: String,
+  guardianName: String,
+  formDate: Date,
+  formSignature: Boolean,
+})
+
+module.exports = mongoose.model('Form', formSchema)
