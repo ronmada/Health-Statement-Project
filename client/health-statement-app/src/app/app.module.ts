b42b47a78d7bf97b878041caf1e0e62902e401ac
Ron Wexler <ronmada@gmail.com>
2020-10-10 18:53:32 +0300

    hy

--- client/health-statement-app/src/app/app.module.ts
+++ client/health-statement-app/src/app/app.module.ts
@@ -5,18 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignatureFormComponent } from './components/signature-form/signature-form.component';
 import { MainFormComponent } from './components/main-form/main-form.component';
-
+import { CanvasWhiteboardModule } from 'ng2-canvas-whiteboard';
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignatureFormComponent,
-    MainFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SignatureFormComponent, MainFormComponent],
+  imports: [BrowserModule, AppRoutingModule, CanvasWhiteboardModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
