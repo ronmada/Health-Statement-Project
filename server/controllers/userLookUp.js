ab4e6987a8c2fbd5af8e521f9cd00bd0f95f84b3
Ron Wexler <ronmada@gmail.com>
2020-10-19 22:17:53 +0300

    kk

--- server/controllers/userLookUp.js
+++ server/controllers/userLookUp.js
@@ -37,7 +37,7 @@ exports.userLookUp = async (req, res, next) => {
     // return res
     //   .status(200)
     //   .json({ error: `Not found user with this ID: ${id}` });
-    next()
+    next();
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
